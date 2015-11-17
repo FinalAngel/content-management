1756a140f0b2e638ba56857b6e58ce9cbc9cc5a0
Angelo Dini <angelo.dini@divio.ch>
2015-11-17 14:48:05 +0100

    adapt settings

--- django-cms/settings.py
+++ django-cms/settings.py
@@ -272,6 +272,8 @@ ALDRYN_STYLE_CLASS_NAMES = (
 # DOCS: https://github.com/divio/djangocms-text-ckeditor
 
 CKEDITOR_SETTINGS = {
-    'stylesSet': 'default:/static/js/addons/ckeditor.wysiwyg.js',
-    'contentsCss': ['/static/css/base.css'],
+    'contentsCss': [
+        '/static/css/bootstrap.min.css',
+        '/static/css/clean-blog.min.css'
+    ],
 }
